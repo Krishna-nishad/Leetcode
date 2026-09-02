b43bd6999310988eff576686bbb1d210a8262b45
Krishna Nishad <133627054+Krishna-nishad@users.noreply.github.com>
2026-09-03 01:03:52 +0530

    Time: 0 ms (100%), Space: 95.2 MB (61.34%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+        // Always binary search on smaller array
+        if (nums1.size() > nums2.size()) {
+            swap(nums1, nums2);
+        }
+
+        int m = nums1.size();
+        int n = nums2.size();
+
+        int low = 0;
+        int high = m;
+
+        int half = (m + n + 1) / 2;
+
+        while (low <= high) {
+
+            int i = low + (high - low) / 2;
+            int j = half - i;
+
+            int left1  = (i == 0) ? INT_MIN : nums1[i - 1];
+            int right1 = (i == m) ? INT_MAX : nums1[i];
+
+            int left2  = (j == 0) ? INT_MIN : nums2[j - 1];
+            int right2 = (j == n) ? INT_MAX : nums2[j];
+
+            // Correct partition
+            if (left1 <= right2 && left2 <= right1) {
+
+                // Odd
+                if ((m + n) % 2 == 1) {
+                    return max(left1, left2);
+                }
+
+                // Even
+                return (max(left1, left2) +
+                        min(right1, right2)) / 2.0;
+            }
+
+            // Too many elements taken from nums1
+            else if (left1 > right2) {
+                high = i - 1;
+            }
+
+            // Too few elements taken from nums1
+            else {
+                low = i + 1;
+            }
+        }
+
+        return 0.0;
+    }
+};
